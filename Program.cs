e9e3df75bfe21b568c9b9eb38041a95621689bc6
Abril <abril.tort@cirvianum.cat>
2024-11-28 22:03:26 +0100

    Nous fitxers actualitzats després de la modificació del Program

--- Program.cs
+++ Program.cs
@@ -16,5 +16,17 @@ class Program
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
+
+        // Crear un altre objecte Vehicle
+        Vehicle vehicle3 = new Vehicle("beetle", "Volkswagen", 1938, 220.0);
+        
+        // Mostrar la informació del segon vehicle
+        vehicle3.MostrarInformacio();
+
+        // Crear un altre objecte Vehicle
+        Vehicle vehicle4 = new Vehicle("Touran", "Volkswagen", 2003, 250.0);
+        
+        // Mostrar la informació del segon vehicle
+        vehicle4.MostrarInformacio();
     }
 }
